dca66994531a7d04e52ad6f5b790446057d7eee7
ankur kushwaha <68402000+ankur-prog@users.noreply.github.com>
2020-09-05 22:12:47 +0200

    Update settings.py
    password hidden.

--- myrealstate/settings.py
+++ myrealstate/settings.py
@@ -151,5 +151,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'teamankur30@gmail.com'
-EMAIL_HOST_PASSWORD = 'Ankur@01'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_USE_TLS = True
